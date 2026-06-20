7d6118bf5bfe4277e63d633f8d513464abb59d47
yugpatel <patelyug2809@gmail.com>
2026-06-20 14:56:22 +0530

    just Commited

---
+++ P1.java
@@ -0,0 +1,18 @@
+class A{
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+class B extends A{
+    void bark(){
+        System.out.println("Dog is barking");
+    }
+}
+
+public class P1 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.eat();
+        b.bark();
+    }
+}
